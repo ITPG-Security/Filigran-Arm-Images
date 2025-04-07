77cd4015bfb910aba641c0811dbe8d1836d60e8f
Ruben van Osch <ruben.van.osch@it-pg.nl>
2025-04-07 12:37:16 +0200

    Updated repo

---
+++ build_images.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# Set the platforms you want to build for
+PLATFORMS="linux/amd64,linux/arm64"
+
+# Base directory (default to current dir if not provided)
+BASE_DIR="${1:-.}"
+
+# Tag prefix (e.g. "myrepo/service")
+TAG_PREFIX="${2:-rvoitpg}"
+
+# Builder name
+BUILDER_NAME="$3"
+
+OPENCTI_VERSION="${4:-false}"
+BUILD_OPENCTI="${5:-false}"
+OPENBAS_VERSION="${6:-false}"
+BUILD_OPENBAS="${7:-false}"
+
+# Ensure buildx builder exists
+if ! docker buildx inspect "$BUILDER_NAME" >/dev/null 2>&1; then
+    echo "❌ Buildx builder '$BUILDER_NAME' does not exist."
+    exit 1
+fi
+
+docker buildx use "$BUILDER_NAME"
+
+# ------------------------
+# Find Dockerfiles recursively
+# ------------------------
+find "$BASE_DIR" -type f -name 'Dockerfile' | while read -r dockerfile; do
+    service_dir=$(dirname "$dockerfile")
+    
+    # Filters out directories that shouldn't be parsed
+    if [[ "$service_dir" =~ (/templates/|/shared/) ]]; then
+        continue
+    fi
+
+    # Extract category and service from the path
+    path_parts=(${service_dir//\// })
+    SERVICE="${path_parts[-1]}"
+    ORIGIONAL_REPO="NOTGOOD"
+    SERVICE_PREFIX="NOTGOOD"
+    if [ "${path_parts[1]}" == "OpenCTI" ]; then
+        if [ "${BUILD_OPENCTI}" == "false" ]; then
+            continue
+        fi
+        ORIGIONAL_REPO="opencti"
+        IMAGE_VERSION=$OPENCTI_VERSION
+        if [ "${path_parts[2]}" == "Connectors" ]; then
+            SERVICE_PREFIX="connector"
+        fi
+    else 
+        if [ "${BUILD_OPENBAS}" == "false" ]; then
+            continue
+        fi
+        ORIGIONAL_REPO="openbas"
+        IMAGE_VERSION=$OPENBAS_VERSION
+        if [ "${path_parts[2]}" == "Collectors" ]; then
+            SERVICE_PREFIX="collector"
+        else
+            SERVICE_PREFIX="injector"
+        fi
+    fi
+
+    # Create image name
+    IMAGE_NAME="${SERVICE_PREFIX}-${SERVICE}"
+    IMAGE_TAG="${TAG_PREFIX}/${IMAGE_NAME}:${IMAGE_VERSION}"
+
+
+    echo "🔨 Building $IMAGE_TAG from $service_dir for platforms: $PLATFORMS"
+
+    #docker buildx build \
+    #    --builder "$BUILDER_NAME" \
+    #    --platform "$PLATFORMS" \
+    #    -t "$IMAGE_TAG" \
+    #    --push \
+    #    "$service_dir"
+
+    echo "✅ Successfully built and pushed $IMAGE_TAG"
+done
